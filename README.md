9293bb4a940b1c1820736bee2a5933f371260725
Grant Heffernan <heffergm@gmail.com>
2014-10-23 12:22:45 -0400

    geonames

--- README.md
+++ README.md
@@ -63,3 +63,6 @@ Bugs
 * due to an issue in Elasticsearch 0.3.10 cookbook, setting `default[:elasticsearch][:version]` inside the attributes file does not take effect
   * current workaround is to override the version in vagrantfile_chef.rb
   * future fix is to move to Elasticsearch 0.3.11 when it's pushed to opscode
+* there's an issue with geonames which will cause the load to always run unless you set `'index_data' => false` in vagrant_chef.rb explicitly
+  * # NOTE: there's a bug here in that the download doesn't write to data_dir, so this will always run at the moment.
+
--- recipes/geonames.rb
+++ recipes/geonames.rb
@@ -34,21 +34,22 @@ end
 #   as an additional guard against a re-load.
 #
 node[:pelias][:geonames][:country_codes].each do |country|
-  # execute "download geonames for #{country}" do
-  #  user    node[:pelias][:user][:name]
-  #  command "./bin/pelias-geonames -d #{country} >#{node[:pelias][:basedir]}/logs/geonames_#{country}.log 2>&1"
-  #  cwd     "#{node[:pelias][:basedir]}/pelias-geonames/current"
-  #  timeout node[:pelias][:geonames][:timeout]
-  #  environment(
-  #    'HOME' => node[:pelias][:user][:home],
-  #    'PELIAS_CONFIG' => "#{node[:pelias][:cfg_dir]}/#{node[:pelias][:cfg_file]}"
-  #  )
-  #  notifies :run, "execute[load geonames for #{country}]", :immediately
-  #  only_if { node[:pelias][:geonames][:index_data] == true && !::File.exist?("#{node[:pelias][:geonames][:data_dir]}/#{country}.zip") }
-  # end
+  execute "download geonames for #{country}" do
+    user    node[:pelias][:user][:name]
+    command "./bin/pelias-geonames -d #{country} >#{node[:pelias][:basedir]}/logs/geonames_#{country}.log 2>&1"
+    cwd     "#{node[:pelias][:basedir]}/pelias-geonames/current"
+    timeout node[:pelias][:geonames][:timeout]
+    environment(
+      'HOME' => node[:pelias][:user][:home],
+      'PELIAS_CONFIG' => "#{node[:pelias][:cfg_dir]}/#{node[:pelias][:cfg_file]}"
+    )
+    notifies :run, "execute[load geonames for #{country}]", :immediately
+    # NOTE: there's a bug here in that the download doesn't write to data_dir, so this will always run at the moment.
+    only_if { node[:pelias][:geonames][:index_data] == true && !::File.exist?("#{node[:pelias][:geonames][:data_dir]}/#{country}.zip") }
+  end
 
   execute "load geonames for #{country}" do
-    action  :run # action :nothing TODO: revert to :nothing when geonames download works
+    action  :nothing
     user    node[:pelias][:user][:name]
     command "./bin/pelias-geonames -i #{country} >#{node[:pelias][:basedir]}/logs/geonames_#{country}.log 2>&1"
     cwd     "#{node[:pelias][:basedir]}/pelias-geonames/current"
@@ -57,6 +58,5 @@ node[:pelias][:geonames][:country_codes].each do |country|
       'HOME' => node[:pelias][:user][:home],
       'PELIAS_CONFIG' => "#{node[:pelias][:cfg_dir]}/#{node[:pelias][:cfg_file]}"
     )
-    only_if { node[:pelias][:geonames][:index_data] == true }
   end
 end
