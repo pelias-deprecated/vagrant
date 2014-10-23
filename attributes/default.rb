795cc16d2edf512c1fc22e952e651a05bce7ac07
Grant Heffernan <heffergm@gmail.com>
2014-10-23 09:24:35 -0400

    java

--- attributes/default.rb
+++ attributes/default.rb
@@ -85,5 +85,8 @@ default[:elasticsearch][:plugins]             = {
   }
 }
 
-# default[:java][:install_flavor]               = 'openjdk'
-# default[:java][:jdk_version]                  = '7'
+default[:java][:install_flavor]                         = 'oracle'
+default[:java][:jdk_version]                            = '7'
+default[:java][:oracle][:accept_oracle_download_terms]  = true
+default[:java][:jdk][:7][:x86_64][:url]                 = 'http://download.oracle.com/otn-pub/java/jdk/7u51-b13/jdk-7u51-linux-x64.tar.gz'
+default[:java][:jdk][:7][:x86_64][:checksum]            = '764f96c4b078b80adaa5983e75470ff2'
