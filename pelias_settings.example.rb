322e649384ef13dfbe3d0390305035b7851f999d
Grant Heffernan <heffergm@gmail.com>
2014-11-04 07:55:24 -0500

    example settings

--- pelias_settings.example.rb
+++ pelias_settings.example.rb
@@ -2,14 +2,14 @@
 #   and setting the environment variable PELIAS_VAGRANTFILE=/path/to/your/file.
 #   This default config will be loaded if you don't set this value.
 #
-# The index_data key will only carry out the task if the
+# the index_data key will only carry out the task if the
 #   underlying guards in the chef recipes return false. For example, if
 #   create_index => true is set for the schema, the schema will be created
 #   on the first provisioning run. If the image is re-provisioned with create_index => true,
 #   the index creation step will be skipped as the recipe will check to see if the
 #   index is already present.
 #
-# Similarly, if index_data => true is set for geonames, quattroshapes or osm, the data will be
+# similarly, if index_data => true is set for geonames, quattroshapes or osm, the data will be
 #   loaded on the initial provisioning run. Subsequent provisioning runs will not attempt to re-index
 #   the data, as the recipe will check for the existence of downloaded data and skip the run if it
 #   already exists.
@@ -22,22 +22,18 @@ Vagrant.configure('2') do |config|
       },
       'pelias' => {
         'schema' => {
-          'revision' => 'master',
           'create_index' => true
         },
         'geonames' => {
-          'revision' => 'master',
           'index_data' => true,
           'country_codes' => [
             'IT'
           ]
         },
         'quattroshapes' => {
-          'revision' => 'master',
           'index_data' => true
         },
         'osm' => {
-          'revision' => 'master',
           'index_data' => true,
           'extracts' => {
             'rome' => 'https://s3.amazonaws.com/metro-extracts.mapzen.com/rome_italy.osm.pbf',
