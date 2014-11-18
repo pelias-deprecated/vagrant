9d65e421560b6b8edd99ac7e9d2bad620ac1b871
Grant Heffernan <heffergm@gmail.com>
2014-11-18 06:58:10 -0500

    bump version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'grant@mapzen.com'
 license          'GPL'
 description      'Installs/configures Pelias in a vagrant environment. Intended for education and development.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.2.0'
 
 %w(
   apt
