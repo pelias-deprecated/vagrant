c71adf17142a3b57d06f55a6f3b391fb9279efdd
Grant Heffernan <heffergm@gmail.com>
2014-12-09 16:00:19 -0500

    merge api and api_service

--- cookbooks/pelias/recipes/api.rb
+++ cookbooks/pelias/recipes/api.rb
@@ -1,6 +1,6 @@
 #
 # Cookbook Name:: pelias
-# Recipe:: api_deploy
+# Recipe:: api
 #
 
 git node[:pelias][:api][:deploy_to] do
@@ -20,4 +20,17 @@ execute 'npm install pelias-api' do
   environment('HOME' => node[:pelias][:api][:deploy_to])
 end
 
-include_recipe 'pelias::api_service'
+include_recipe 'runit::default'
+
+runit_service 'pelias-api' do
+  action          [:enable, :start]
+  log             true
+  default_logger  true
+  sv_timeout      60
+  subscribes      :restart, "template[#{node[:pelias][:cfg_dir]}/#{node[:pelias][:cfg_file]}", :delayed
+  env(
+    'PELIAS_CONFIG' => "#{node[:pelias][:cfg_dir]}/#{node[:pelias][:cfg_file]}",
+    'NODE_ENV'      => "#{node[:pelias][:node_env]}",
+    'PORT'          => "#{node[:pelias][:api][:port]}"
+  )
+end
