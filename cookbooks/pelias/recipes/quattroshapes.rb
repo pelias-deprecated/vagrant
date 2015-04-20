fc38644122c86af601032225d3c4680adcca7e58
Grant Heffernan <heffergm@gmail.com>
2015-04-20 16:51:11 -0400

    fix subscribes bug in quattroshapes

--- cookbooks/pelias/recipes/quattroshapes.rb
+++ cookbooks/pelias/recipes/quattroshapes.rb
@@ -48,7 +48,7 @@ node[:pelias][:quattroshapes][:alpha3_country_codes].each do |country|
       command     "node example/runme.js #{type} #{country} >#{node[:pelias][:basedir]}/logs/quattroshapes_#{country}_#{type}.out 2>#{node[:pelias][:basedir]}/logs/quattroshapes_#{country}_#{type}.err"
       cwd         "#{node[:pelias][:basedir]}/quattroshapes-pipeline/current"
       timeout     node[:pelias][:quattroshapes][:timeout]
-      subscribes  :run, "execute[extract quattroshapes for #{country}]", :immediately
+      subscribes  :run, 'execute[extract quattroshapes]', :immediately
       environment(
         'PELIAS_CONFIG' => "#{node[:pelias][:cfg_dir]}/#{node[:pelias][:cfg_file]}"
       )
