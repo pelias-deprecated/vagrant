1dcea4b69d368ad35f8a7f89cd364d8d551ef7e3
Julian Simioni <julian@mapzen.com>
2016-08-12 11:35:14 -0400

    Update node version

--- cookbooks/pelias/attributes/external.rb
+++ cookbooks/pelias/attributes/external.rb
@@ -2,7 +2,7 @@
 #
 
 # nodejs
-default[:nodejs][:version]                      = '0.12.2'
+default[:nodejs][:version]                      = '4.4.7'
 default[:nodejs][:checksum_linux_x64]           = '4e1578efc2a2cc67651413a05ccc4c5d43f6b4329c599901c556f24d93cd0508'
 
 # elasticsearch
